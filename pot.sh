606f8373b7eaee1da51c439967bdd8e0e57f0b4c
Kaspars <hi@kaspars.net>
2014-04-06 17:18:03 +0300

    Allow both plugin and theme pot files to be generated

--- pot.sh
+++ pot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+TYPE=${1-'wp-plugin'}
 SLUG=$(basename $(dirname $PWD))
 
-# Make sure we are on the master branch
-export GIT_DIR=../git/.git
-git checkout -f master
+# TODO: Make sure we are on the master branch
+# export GIT_DIR=../git/.git
+# git checkout -f master
 
-php ./wp-develop/tools/i18n/makepot.php wp-plugin ../git ../git/languages/$SLUG.pot
+php ./wp-develop/tools/i18n/makepot.php $TYPE ../git ../git/languages/$SLUG.pot
